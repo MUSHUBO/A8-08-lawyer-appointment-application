aa98be768d8a6794be7b635db241f2a7da4ab496
MUSHUBO <shubo3132@gmail.com>
2025-04-22 04:57:44 +0600

    added the footer secion

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar w-10/12 mx-auto border-b border-gray-200">
+        <div className="navbar w-10/12 mx-auto border-b border-gray-200 p-3">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='my-20'>
             <h3>hello i am Home</h3>
         </div>
     );
